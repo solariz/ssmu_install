f1b9754b2122dc99c98912f011bb904f916cce82
Marco G <solariz@users.noreply.github.com>
2018-04-27 17:24:32 +0200

    Update libsodium.sh

--- patch/libsodium.sh
+++ patch/libsodium.sh
@@ -3,13 +3,14 @@
 # If you suppose your server supports chacha and salsa,
 # this script must be run.
 
-wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.15.tar.gz
-tar xvzf libsodium-1.0.15.tar.gz
-cd libsodium-1.0.15
+wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
+tar xvzf LATEST.tar.gz
+cd libsodium-stable
 ./configure
-make
+make -j2
 make install
 cd ..
 
 echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
+ldconfig -p | grep libsodium
